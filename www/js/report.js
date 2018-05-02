b49f888117aadf1aeda3f784ccd8a6e9eda06470
Unknown <ben.004@hotmail.com>
2018-05-02 21:03:44 +0700

    picker format and report, history style fix

--- www/js/report.js
+++ www/js/report.js
@@ -1,10 +1,75 @@
 $(document).ready(function () {
 
-    $('.datepicker').datepicker();
-    $('.timepicker').timepicker();
+    $('.datepicker').datepicker({
+        i18n: {
+            months: [
+                'มกราคม',
+                'กุมภาพันธ์',
+                'มีนาคม',
+                'เมษายน',
+                'พฤษภาคม',
+                'มิถุนายน',
+                'กรกฏาคม',
+                'สิงหาคม',
+                'กันยายน',
+                'ตุลาคม',
+                'พฤศจิกายน',
+                'ธันวาคม'
+            ],
+            monthsShort: [
+                'ม.ค.',
+                'ก.พ.',
+                'มี.ค.',
+                'เม.ย',
+                'พ.ค.',
+                'มิ.ย.',
+                'ก.ค.',
+                'ส.ค.',
+                'ก.ย.',
+                'ต.ค.',
+                'พ.ย.',
+                'ธ.ค.'
+            ],
+            weekdays:[
+                'อาทิตย์',
+                'จันทร์',
+                'อังคาร',
+                'พุธ',
+                'พฤหัสบดี',
+                'ศุกร์',
+                'เสาร์'
+            ],
+            weekdaysShort: [
+                'อา',
+                'จ',
+                'อ',
+                'พ',
+                'พฤ',
+                'ศ',
+                'ส'
+            ],
+            weekdaysAbbrev: [
+                'อ',
+                'จ',
+                'อ',
+                'พ',
+                'พ',
+                'ศ',
+                'ส'
+            ]
+        },
+        format: 'd mmmm yyyy',
+        showClearBtn: true,
+        showDaysInNextAndPreviousMonths: true,
+        setDefaultDate: true
+        
+    });
+    $('.timepicker').timepicker({
+        defaultTime: 'now',
+        twelveHour: false,
+        showClearBtn: true        
+    });
     $('select').formSelect();
 
-    $('#textarea1').val();
-    M.textareaAutoResize($('#textarea1'));
 });
 
